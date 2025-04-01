2fafef535e5dc707f503a2dbe8c25cef4f65091b
bq22005 <bq22005@shibaura-it.ac.jp>
2025-04-01 15:09:31 +0900

    Add functions to upload file

--- src/app/components/layouts/ConvertForm.tsx
+++ src/app/components/layouts/ConvertForm.tsx
@@ -3,14 +3,32 @@
 import styles from "./ConvertForm.module.css";
 import { Button } from "@/app/components/elements/Button";
 import { useState } from "react";
+import { postImage } from "@/app/lib/postImage";
 
 export function ConvertForm() {
   const extFrom: string[] = ["png"];
   const extTo: string[] = ["svg"];
   
   const [message, setMessage] = useState("ファイルをアップロードしてください");
-  const [selectedExt, setSelectedExt] = useState("");
+  const [selectedExt, setSelectedExt] = useState<string>("");
   const [isDisabled, setIsDisabled] = useState(false);
+  const [image, setImage] = useState<File | null>(null);
+  const [convertedUrl, setConvertedUrl] = useState<string | null>(null);
+
+  const handleChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setImage(file);
+    }
+  };
+
+  const handleUploadFile = async () => {
+    if (!image) {
+      return;
+    }
+    const url = await postImage(image);
+    setConvertedUrl(url);
+  };
 
   return (
     <div className={styles.convertForm}>
@@ -23,6 +41,7 @@ export function ConvertForm() {
               onChange={(e) => setSelectedExt(e.target.value)}
               value={selectedExt}
             >
+              <option value="">select extension</option>
               {extFrom.map((ext) => (
                 <option key={ext} value={ext}>{ext}</option>
               ))}
@@ -31,18 +50,23 @@ export function ConvertForm() {
           <div className={styles.to}>
             <h2 className={styles.formTitle}>To</h2>
             <select className={styles.dropdown}>
+              <option value="">select extension</option>
               {extTo.map((ext) => (
                 <option key={ext} value={ext}>{ext}</option>
               ))}
             </select>
           </div>
         </div>
-        <div className={styles.uploadContainer}>
-          <input
-            className={styles.upload}
-            type="file"
-            accept={selectedExt ? `image/${selectedExt}` : "image/*"}
-          />
+        <div className={styles.uploadForm}>
+          <div className={styles.uploadContainer}>
+            <input
+              className={styles.upload}
+              type="file"
+              accept={selectedExt ? `image/${selectedExt}` : "image/*"}
+              onChange={handleChangeFile}
+            />
+          </div>
+          <Button onClick={handleUploadFile}>アップロード</Button>
         </div>
         <div className={styles.downloadForm}>
           <h2 className={styles.message}>{message}</h2>
---
+++ src/app/lib/postImage.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const apiUrl = process.env.NEXT_PUBLIC_API_ENDPOINT;
+
+export const postImage = async (imgFile: File) => {
+  const formData = new FormData();
+  formData.append("image", imgFile);
+
+  try {
+    const res = await axios.post(`${apiUrl}/convert`, imgFile);
+    return res.data;
+    
+  } catch (error) {
+    console.error("Failed to post image.", error);
+    return null;
+  }
+}
